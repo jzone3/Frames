7e453196f73f7c53ca04acd7a148dcbfc5c1b0e1
Paras Modi <parasm346@gmail.com>
2013-10-12 22:40:48 -0400

    Blobstore stuff

--- frames-app/main.py
+++ frames-app/main.py
@@ -18,11 +18,13 @@ import os
 import logging
 import jinja2
 import json
+import urllib
 import webapp2
 from google.appengine.ext import db
 from google.appengine.api import memcache
 from google.appengine.api import datastore_errors
 from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
 
 try:
   # When deployed
@@ -37,54 +39,62 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 class BaseHandler(webapp2.RequestHandler):
-	'''Parent class for all handlers, shortens functions'''
-	def write(self, content):
-		return self.response.out.write(content)
+    '''Parent class for all handlers, shortens functions'''
+    def write(self, content):
+        return self.response.out.write(content)
 
-	def rget(self, name):
-		'''Gets a HTTP parameter'''
-		return self.request.get(name)
+    def rget(self, name):
+        '''Gets a HTTP parameter'''
+        return self.request.get(name)
 
-	def logged_in(self, email_cookie = None):
-		'''Checks if login cookie is valid (authenticates user)'''
-		email_cookie = self.request.cookies.get(LOGIN_COOKIE_NAME, '')
-		if email_cookie:
-			email, hashed_email = email.split("|")
-			if email and hashed_email and hash_str(email) == hashed_email:
-				return True
-			else:
-				self.delete_cookie(LOGIN_COOKIE_NAME)
-				return False
-		return False
+    def logged_in(self, email_cookie = None):
+        '''Checks if login cookie is valid (authenticates user)'''
+        email_cookie = self.request.cookies.get(LOGIN_COOKIE_NAME, '')
+        if email_cookie:
+            email, hashed_email = email.split("|")
+            if email and hashed_email and hash_str(email) == hashed_email:
+                return True
+            else:
+                self.delete_cookie(LOGIN_COOKIE_NAME)
+                return False
+        return False
 
-class MainHandler(BaseHandler):
-    def get(self):
-        self.response.write('Hello world!')
+class MainHandler(webapp2.RequestHandler):
+  def get(self):
+    upload_url = blobstore.create_upload_url('/upload')
+    self.response.out.write('<html><body>')
+    self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+    self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
+        name="submit" value="Submit"> </form></body></html>""")
 
-class SendImage(BaseHandler):
-    def get(self):
-        self.response.out.write('<html><body>')
-        self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
-        self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
-                                name="submit" value="Submit"> </form></body></html>""")
-    def post(self):
-        upload_url = blobstore.create_upload_url_async('/upload')
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+  def post(self):
+    upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
+    blob_info = upload_files[0]
+    self.redirect('/serve/%s' % blob_info.key())
+
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+  def get(self, resource):
+    resource = str(urllib.unquote(resource))
+    blob_info = blobstore.BlobInfo.get(resource)
+    self.send_blob(blob_info)
 
 class CreateAccount(BaseHandler):
     def post(self):
-    	username = self.rget('username')
-    	psswrd = self.rget('password')
-    	verify = self.rget('verify')
-    	email = self.rget('email')
-    	self.response.headers['Content-Type'] = 'application/json'
+        username = self.rget('username')
+        psswrd = self.rget('password')
+        verify = self.rget('verify')
+        email = self.rget('email')
+        self.response.headers['Content-Type'] = 'application/json'
         returned = create_account(username=username, password=psswrd, verify=verify, email=email)
         if 'cookie' in returned:
-        	self.response.headers.add_header('Set-Cookie', returned['cookie'])
+            self.response.headers.add_header('Set-Cookie', returned['cookie'])
         del returned['cookie']
         self.write(json.dumps(returned))
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/send_image', SendImage),
-    ('/create_account', CreateAccount),
-], debug=True)
+
+app = webapp2.WSGIApplication([('/', MainHandler),
+                                ('/create_account', CreateAccount),
+                               ('/upload', UploadHandler),
+                               ('/serve/([^/]+)?', ServeHandler)],
+                              debug=True)
