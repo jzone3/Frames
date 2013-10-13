8736e730ae6dcbd2422e377e92305dfa09e613c6
Paras Modi <parasm346@gmail.com>
2013-10-13 06:04:09 -0400

    Fixed Post

--- frames-app/main.py
+++ frames-app/main.py
@@ -60,36 +60,14 @@ class BaseHandler(webapp2.RequestHandler):
                 return False
         return False
 class Picture(db.Model):
-    picture = db.StringProperty(required = True)
+    picture = db.TextPropertyy(required = True)
     location = db.StringProperty(required=False)
     created = db.DateTimeProperty(auto_now_add = True)
     last_modified = db.DateTimeProperty(auto_now_add=True)
 class MainHandler(BaseHandler):
     def get(self):
-<<<<<<< HEAD
-        upload_url = blobstore.create_upload_url('/upload')
-        self.response.out.write('<html><body>')
-        self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
-        self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
-            name="submit" value="Submit"> </form></body></html>""")
+        self.write("hi david")
 
-    def post(self, data):
-        upload_url = blobstore.create_upload_url('/upload')
-        u = urllib.Request(upload_url,data,{'Content-Type':'multipart'})
-
-class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
-	def post(self):
-		upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
-		blob_info = upload_files[0]
-		self.redirect('/serve/%s' % blob_info.key())
-
-class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
-	def get(self, resource):
-		resource = str(urllib.unquote(resource))
-		blob_info = blobstore.BlobInfo.get(resource)
-		self.send_blob(blob_info)
-=======
-        self.write('hello david')
 class Image(BaseHandler):
     def get(self):
         self.write('i hate blobstore')
@@ -97,7 +75,6 @@ class Image(BaseHandler):
         picture = self.rget('picture')
         p = Picture(picture=picture)
         p.put()
->>>>>>> de903e2ac9575b66d5ef7b257ea2f351f2766359
 
 class CreateAccount(BaseHandler):
     def post(self):
@@ -125,11 +102,6 @@ class GetFeed(BaseHandler):
 
 app = webapp2.WSGIApplication([('/', MainHandler),
                                 ('/create_account', CreateAccount),
-<<<<<<< HEAD
-                               ('/upload', UploadHandler),
-                               ('/serve/([^/]+)?', ServeHandler),
                                ('/get_feed', GetFeed)],
-=======
                                 ('/image',Image),],
->>>>>>> de903e2ac9575b66d5ef7b257ea2f351f2766359
                               debug=True)
